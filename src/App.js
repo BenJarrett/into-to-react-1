32d6095c010a736c10e7c86e4fbd9577f08b6638
Ben Jarrett <b.cl.jarrett@gmail.com>
2021-04-17 13:56:01 -0500

    added more

--- src/App.js
+++ src/App.js
@@ -4,12 +4,34 @@ import Counter from './Counter';
 
 
 function App() { 
+
+  const array = [
+    {
+      appName: "Counter 1",
+      counterValue: 200
+    },
+    {
+      appName: "Counter 6",
+      counterValue: 0
+    },
+    {
+      appName: "Counter 3",
+      counterValue: 100
+    },
+    {
+      appName: "Counter 15",
+      counterValue: 50
+    }
+  ];
+
+
+
   return (
     <>
-  <Counter appName="Counter 1" counterValue={(200)}/>
-  <Counter appName="Counter 2" counterValue={(200)}/>
-  <Counter appName="Counter 3" counterValue={(200)}/>
+    {array.map((counter, i) => <Counter key={i} appName={counter.appName} counterValue={counter.counterValue}/>)}
+  
   </>
   )
 }
+
 export default App;
